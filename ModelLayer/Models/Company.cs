616d19d2a7a90f069f973a918bf2e785647280b3
Caddeo <poss_en@hotmail.com>
2015-02-03 09:54:28 +0100

    Swagbus

---
+++ ModelLayer/Models/Company.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ModelLayer.Models
+{
+    public class Company
+    {
+
+        private string _Name;
+
+        public string Name { get { return _Name; } }
+
+        public Company()
+        {
+
+        }
+
+        public Company(string Name)
+        {
+            this._Name = Name;
+        }
+    }
+}
